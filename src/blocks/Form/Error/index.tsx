f572db2a7c4cda67c6af4f0caaec2ff3b38957e4
Michael Owuor <michael.owuor@gmail.com>
2025-07-01 14:53:57 +0300

    chore: miscellaneous updates and fixes (#1)
    * refactor: importMap for consistency and readability
    
    * refactor: update Prettier configuration and format code for consistency
    
    * refactor: update Prettier configuration and add prettier-plugin-tailwindcss for improved formatting
    
    * feat: integrate Resend email service into payload configuration for enhanced email functionality
    
    * refactor: homepage content and metadata
    
    * refactor: code style for consistency and readability across multiple utility files.

--- src/blocks/Form/Error/index.tsx
+++ src/blocks/Form/Error/index.tsx
@@ -1,15 +1,13 @@
-'use client'
+"use client";
 
-import * as React from 'react'
-import { useFormContext } from 'react-hook-form'
+import * as React from "react";
+import { useFormContext } from "react-hook-form";
 
 export const Error = ({ name }: { name: string }) => {
-  const {
-    formState: { errors },
-  } = useFormContext()
-  return (
-    <div className="mt-2 text-red-500 text-sm">
-      {(errors[name]?.message as string) || 'This field is required'}
-    </div>
-  )
-}
+	const {
+		formState: { errors },
+	} = useFormContext();
+	return (
+		<div className="mt-2 text-sm text-red-500">{(errors[name]?.message as string) || "This field is required"}</div>
+	);
+};
