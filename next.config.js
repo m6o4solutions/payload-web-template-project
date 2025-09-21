a63e462c865b7518249bcba4982f6d16da834605
Michael Owuor <michael.owuor@gmail.com>
2025-09-21 15:36:54 +0300

    feat: implement new Next.js configuration and remove old files (#19)

--- next.config.js
+++ next.config.js
@@ -1,31 +1,29 @@
-import { NextConfig } from "next";
 import { withPayload } from "@payloadcms/next/withPayload";
-import redirects from "./redirects";
+
+import redirects from "./redirects.js";
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
 	? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-	: process.env.__NEXT_PRIVATE_ORIGIN || "http://localhost:3000";
+	: undefined || process.env.__NEXT_PRIVATE_ORIGIN || "http://localhost:3000";
 
 /** @type {import('next').NextConfig} */
-const nextConfig: NextConfig = {
+const nextConfig = {
 	images: {
 		qualities: [25, 50, 75, 100],
 		remotePatterns: [
 			...[NEXT_PUBLIC_SERVER_URL].map((item) => {
-				// Ensure item is a string before creating a URL object
-				const url = new URL(item as string);
+				const url = new URL(item);
 
 				return {
 					hostname: url.hostname,
-					protocol: url.protocol.replace(":", "") as "http" | "https", // Added a type cast for the protocol
-					pathname: "**", // This is the fix: added the pathname property
+					protocol: url.protocol.replace(":", ""),
 				};
 			}),
 		],
 	},
 	output: "standalone",
 	reactStrictMode: true,
-	redirects: redirects,
+	redirects,
 	webpack: (webpackConfig) => {
 		webpackConfig.resolve.extensionAlias = {
 			".cjs": [".cts", ".cjs"],
